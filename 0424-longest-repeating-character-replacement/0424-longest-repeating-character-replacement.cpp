9fe95c593d9b27a4300edcca80d57c8883858120
Juman Saikia <saikiajuman098@gmail.com>
2026-08-09 11:54:18 +0530

    Time: 10 ms (31.72%), Space: 11 MB (19.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,16 +4,15 @@ public:
         int n = s.size();
         int maxlenght = 0;
         int maxf = 0;
-        int cnt = 1;
         unordered_map<char , int> mp;
         int l = 0 , r = 0;
         while(r < n){
             mp[s[r]]++;
-            maxf  = max(maxf , mp[s[r]]);
+            maxf = max(maxf , mp[s[r]]);
             if(r - l + 1 - maxf <= k){
                 maxlenght = max(maxlenght , r - l + 1);
             }
-            if(r - l + 1 - maxf > k){
+            else{
                 mp[s[l]]--;
                 l++;
             }
