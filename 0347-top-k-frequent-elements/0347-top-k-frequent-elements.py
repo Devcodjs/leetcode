96910e343492b6aa8c889cc48d140e1551c8fed7
Juman Saikia <saikiajuman098@gmail.com>
2026-08-11 23:10:20 +0530

    Time: 19 ms (17.58%), Space: 20.1 MB (5.99%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        n = len(nums)
+        mp = {}
+        for e in nums:
+            mp[e] = mp.get(e , 0)+1
+        ans = [[] for _ in range(n + 1)] 
+        for key , val in mp.items():
+            ans[val].append(key)
+        
+        res = []
+        for i in range(n , -1  , -1):
+            for j in ans[i]:
+                if k > 0:
+                    res.append(j)
+                    k -= 1
+                    if k == 0:break
+        return res
+        
