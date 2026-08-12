54274104b3bc11231058210c8941f96dca36a8f5
Juman Saikia <saikiajuman098@gmail.com>
2026-08-12 22:25:06 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.65%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
-    bool f(vector<int>& dp , vector<int>& nums , int i){
-        int n = nums.size();
-        if(i >= n - 1) return true;
-        if( dp[i] != -1) return dp[i];
-        if(nums[i] == 0) return dp[i] = false;
-        for(int j = i + 1 ; j <= i + nums[i] ; j++){
-            if(f(dp , nums , j)) return dp[i] =  true;
-        }
-        return dp[i] = false;
-    }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n + 1, 0);
-        if(n == 1) return true;
-        dp[0] = 1;
-        for(int i = 0 ; i < n ; i++){
-            if(!dp[i]) continue;
-            
-            for(int j = i + 1 ; j <= i + nums[i] && j < n ; j++){
-                dp[j] = 1;
-            }
+        int fardest = 0;
+        for(int i = 0 ; i < n ;i++){
+            fardest = max(fardest , i + nums[i]);
+            if(fardest == n - 1) return true;
+            if(fardest == i) return false;
         }
-        return dp[n - 1];
+        return true;
     }
 };
