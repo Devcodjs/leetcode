0fd6679ab232a1469b08f80c070d69c5e8901a58
Juman Saikia <saikiajuman098@gmail.com>
2026-08-12 22:18:09 +0530

    Time: 403 ms (11.32%), Space: 54.9 MB (9.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -12,7 +12,16 @@ public:
     }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n , -1);
-        return f( dp , nums , 0);
+        vector<int> dp(n + 1, 0);
+        if(n == 1) return true;
+        dp[0] = 1;
+        for(int i = 0 ; i < n ; i++){
+            if(!dp[i]) continue;
+            
+            for(int j = i + 1 ; j <= i + nums[i] && j < n ; j++){
+                dp[j] = 1;
+            }
+        }
+        return dp[n - 1];
     }
 };
