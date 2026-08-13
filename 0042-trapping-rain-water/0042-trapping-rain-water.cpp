b7884f43f1651436bc3ad87969a5d081caf4e237
Juman Saikia <saikiajuman098@gmail.com>
2026-08-13 21:49:40 +0530

    Time: 0 ms (100%), Space: 26.1 MB (43.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& nums){
         int n = nums.size();
-        int l = 0 , r = 0;
+        int l = 0 , r = n - 1;
         int ans = 0;
-        vector<int> leftMax(n , -1) , rightMax(n , -1);
-        int temp = nums[0];
-        for(int i = 1 ; i < n ; i++){
-            if(temp > nums[i]){
-                leftMax[i] = temp;
-            }
-            temp = max(temp , nums[i]);
-        }
-        temp = nums[n - 1];
-        for(int i = n - 2 ; i >= 0 ; i--){
-            if(temp > nums[i]){
-                rightMax[i] = temp;
-            }
-            temp = max(temp , nums[i]);
-        }
-
-        
-        for(int i =  0 ; i < n ; i++){
-            if(leftMax[i] != -1 && rightMax[i] != -1){
-                ans += (min(leftMax[i] , rightMax[i]) - nums[i]);
+        int leftM = 0  , rightM = 0;
+        while(l <= r){
+            if(nums[l] < nums[r]){
+                if(leftM >= nums[l]){
+                    ans += leftM - nums[l];
+                }else{
+                    leftM = nums[l];
+                }
+                l++;
+            }else{
+                if(rightM > nums[r]){
+                    ans += rightM - nums[r];
+                }else{
+                    rightM = nums[r];
+                }
+                r--;
             }
         }
         return ans;
