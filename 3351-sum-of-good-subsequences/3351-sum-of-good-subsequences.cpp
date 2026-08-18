2df69ffea9272e203191b22923efdc17449d455c
Juman Saikia <saikiajuman098@gmail.com>
2026-08-19 01:23:50 +0530

    Time: 586 ms (33.33%), Space: 303.1 MB (20%) - LeetHub

--- 3351-sum-of-good-subsequences/3351-sum-of-good-subsequences.cpp
+++ 3351-sum-of-good-subsequences/3351-sum-of-good-subsequences.cpp
@@ -5,21 +5,20 @@ public:
         int mx = 100001;
         vector<long long> dp(mx) , sum(mx);
         for(auto &num : nums){
-            long long cnt = 1;
-            long long s = num;
+            long long leftCnt = num > 0 ? dp[num - 1] : 0;
+            long long leftSum = num > 0 ? sum[num - 1] : 0;
 
-            if(num - 1 >= 0){
-                cnt = (cnt + dp[num - 1]) % MOD;
-                s = (s + sum[num - 1] + (num * dp[num - 1]) % MOD) % MOD;
-            }
+            long long rightCnt = num + 1 < mx ? dp[num + 1] : 0;
+            long long rightSum = num + 1 < mx ? sum[num + 1] : 0;
 
-            if(num + 1 < mx){
-                cnt = (cnt + dp[num + 1]) % MOD;
-                s = (s + sum[num + 1] + (num * dp[num + 1]) % MOD) % MOD;
-            }
+            long long newCnt = (1 + leftCnt + rightCnt) % MOD;
 
-            dp[num] = (dp[num] + cnt) % MOD;
-            sum[num] = (sum[num] + s) % MOD;
+            long long newSum = num;
+            newSum = (newSum + leftSum + num * leftCnt) % MOD;
+            newSum = (newSum + rightSum + num * rightCnt) % MOD;
+
+            dp[num] = (dp[num] + newCnt) % MOD;
+            sum[num] = (sum[num] + newSum) % MOD;
         }
         long long ans = 0;
         for(int i = 0 ; i < mx ; i++){
