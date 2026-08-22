eee966d213d0ba0a522a6b31c00cd538eb88370b
Juman Saikia <saikiajuman098@gmail.com>
2026-08-22 09:34:48 +0530

    Time: 0 ms (100%), Space: 20.2 MB (46.25%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& dp , vector<int>& nums,  int i , int buy){
-        int n = nums.size();
-        if(i >= n) return 0;
-        if(dp[i][buy] != -1) return dp[i][buy];
-        int take = 0;
-        if(buy == 0){
-            take = max(-nums[i] + f(dp , nums , i + 1 , 1)  , f(dp , nums, i +1 , 0));
-        }else{
-            take = max(nums[i] + f(dp , nums , i + 1 , 0) , f(dp , nums, i+ 1 , 1));
-        }
-        int notTake = f(dp , nums , i + 1 , buy);
-        return dp[i][buy] = max(take , notTake);
-    }
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
-        vector<int> cur(n + 1, 0) , next(n + 1 , 0);
+        int buy = 0 , sell = 0;
         for(int i = n - 1; i >= 0 ; i--){
-            for(int j = 0 ; j < 2 ; j++){
-                int take = 0;
-                if(j == 0){
-                    take = max(-nums[i] + next[1] , next[0]);
-                }else{
-                    take = max(nums[i] + next[0] , next[1]);
-                }
-                int notTake = next[j];
-                cur[j] = max(take , notTake);
-            }
-            next = cur;
+            int newb = max(-nums[i] + sell , buy);
+            int news = max(nums[i] + buy , sell);
+            sell = news;
+            buy = newb;
         }
      
-        return cur[0];
+        return buy;
     }
 };
