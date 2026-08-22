6f5404fae37f896fa34d6d2c12fe3aa8f159bea9
Juman Saikia <saikiajuman098@gmail.com>
2026-08-22 08:55:25 +0530

    Time: 16 ms (8.11%), Space: 27.9 MB (8.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& dp, vector<int>& prices, int i,
-          int turn) { // turn -> 0 = buy , 1 ->
-        int n = prices.size();
-        if (i >= n)
-            return 0;
-        if (dp[i][turn] != -1)
-            return dp[i][turn];
+    int f(vector<vector<int>>& dp , vector<int>& nums,  int i , int buy){
+        int n = nums.size();
+        if(i >= n) return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
         int take = 0;
-        if (turn == 0) {
-            take = max(-prices[i] + f(dp, prices, i + 1, 1),
-                       f(dp, prices, i + 1, 0));
-        } else {
-            take = max(prices[i] + f(dp, prices, i + 1, 0),
-                       f(dp, prices, i + 1, 1));
+        if(buy == 0){
+            take = max(-nums[i] + f(dp , nums , i + 1 , 1)  , f(dp , nums, i +1 , 0));
+        }else{
+            take = max(nums[i] + f(dp , nums , i + 1 , 0) , f(dp , nums, i+ 1 , 1));
         }
-        return dp[i][turn] = take;
+        int notTake = f(dp , nums , i + 1 , buy);
+        return dp[i][buy] = max(take , notTake);
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-        vector<int> cur(n+1 , 0) , next(n+1 , 0);
-        cur[n] = 0 ;
-        next[n] = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            cur[0] = max(-prices[i] + next[1], next[0]);
-            cur[1] = max(prices[i] + next[0], next[1]);
-            next = cur;
-        }
-        return cur[0];
+        vector<vector<int>> dp(n , vector<int>(2 , -1));
+        return f(dp , prices , 0 , 0);
     }
 };
