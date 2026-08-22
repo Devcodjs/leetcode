1a0cb93dd9dee8671611349c2de7978e85d3a7aa
Juman Saikia <saikiajuman098@gmail.com>
2026-08-22 09:04:34 +0530

    Time: 12 ms (24.23%), Space: 24.7 MB (34.39%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -13,9 +13,22 @@ public:
         int notTake = f(dp , nums , i + 1 , buy);
         return dp[i][buy] = max(take , notTake);
     }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n , vector<int>(2 , -1));
-        return f(dp , prices , 0 , 0);
+    int maxProfit(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1 , vector<int>(2 , 0));  
+        for(int i = n - 1; i >= 0 ; i--){
+            for(int j = 0 ; j < 2 ; j++){
+                int take = 0;
+                if(j == 0){
+                    take = max(-nums[i] + dp[i + 1][1] , dp[i + 1][0]);
+                }else{
+                    take = max(nums[i] + dp[i+1][0] , dp[i + 1][1]);
+                }
+                int notTake = dp[i + 1][j];
+                dp[i][j] = max(take , notTake);
+            }
+        }
+     
+        return dp[0][0];
     }
 };
