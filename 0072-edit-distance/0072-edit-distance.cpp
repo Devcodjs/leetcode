ed6df19428bf45ad62e8d1005aad974d15f4faf7
Juman Saikia <saikiajuman098@gmail.com>
2026-08-22 19:23:12 +0530

    Time: 10 ms (34.1%), Space: 13.4 MB (32.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,32 +1,57 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& dp , string& s, string& t ,  int i , int j){
-        int n = s.size() , m = t.size();
-        if(i >= n){
+    int f(vector<vector<int>>& dp, string& s, string& t, int i, int j) {
+        int n = s.size(), m = t.size();
+        if (i >= n) {
             return j < m ? m - j : 0;
         }
-        if(j >= m){
+        if (j >= m) {
             return i < n ? (n - i) : 0;
         }
-        if(dp[i][j] != -1) return dp[i][j];
-        int cnt1 = 0 , cnt2 = 0 , cnt3 = 0;
-        if(s[i] == t[j]) return dp[i][j] =  f(dp , s, t , i + 1 , j + 1);
-        else{
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        if (s[i] == t[j])
+            return dp[i][j] = f(dp, s, t, i + 1, j + 1);
+        else {
             // replace the char..
-            cnt1 =  1 + f(dp , s , t , i + 1 , j + 1);
+            cnt1 = 1 + f(dp, s, t, i + 1, j + 1);
 
             // delete the char...
-            cnt2 =  1 + f(dp , s , t , i + 1 , j);
+            cnt2 = 1 + f(dp, s, t, i + 1, j);
 
-            //add the char....
-            cnt3 =  1 + f(dp , s , t , i , j + 1);
-
+            // add the char....
+            cnt3 = 1 + f(dp, s, t, i, j + 1);
         }
-        return dp[i][j] = min({cnt1 , cnt2 , cnt3});
+        return dp[i][j] = min({cnt1, cnt2, cnt3});
     }
     int minDistance(string word1, string word2) {
-        int n = word1.size() , m = word2.size();
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-        return f(dp , word1 , word2 , 0 , 0);
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for(int i = 0; i <= n ; i++){
+            dp[i][m] = n - i;
+        }
+        for(int j = 0 ; j <= m ; j++){
+            dp[n][j] = m - j;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+                if (word1[i] == word2[j])
+                    dp[i][j] = dp[i+1][j+1];
+                else {
+                    // replace the char..
+                    cnt1 = 1 + dp[i+1][j+1];
+
+                    // delete the char...
+                    cnt2 = 1 + dp[i+1][j];
+
+                    // add the char....
+                    cnt3 = 1 + dp[i][j+1];
+                    dp[i][j] = min({cnt1, cnt2, cnt3});
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
