2ecee94a866407a7e765bdb00a5d5e378af8e1c5
Juman Saikia <saikiajuman098@gmail.com>
2026-08-22 20:12:17 +0530

    Time: 0 ms (100%), Space: 10.2 MB (88.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -9,22 +9,29 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp1(n+2  , 0) , dp2(n + 2 , 0);
+        int next = 0 , nNext = 0;
         if(n == 1) return nums[0];
         int maxi = 0;
         for(int i = n - 1 ; i >= 1 ; i--){
-            int take = nums[i] + dp1[i + 2];
-            int notTake = dp1[i + 1];
-            dp1[i] = max(take , notTake);
+            int cur = 0;
+            int take = nums[i] + nNext;
+            int notTake = next;
+            cur = max(take , notTake);
+            nNext =  next;
+            next = cur;
         }
-        maxi = max(maxi , dp1[1]);
+        maxi = max(maxi , next);
+        next = 0 , nNext = 0;
         int maxi2 = 0;
         for(int i = n - 2 ; i >= 0 ; i--){
-            int take = nums[i] + dp2[i + 2];
-            int notTake = dp2[i + 1];
-            dp2[i] = max(take , notTake);
+            int cur = 0;
+            int take = nums[i] + nNext;
+            int notTake = next;
+            cur = max(take , notTake);
+            nNext = next;
+            next = cur;
         }
-        maxi2 = max(maxi2 , dp2[0]);
+        maxi2 = max(maxi2 , next);
         return max(maxi , maxi2);
     }
 };
