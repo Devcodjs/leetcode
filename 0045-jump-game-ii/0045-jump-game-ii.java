e50c058cb4e21da2dac59481c3695c606973319d
Juman Saikia <saikiajuman098@gmail.com>
2026-08-23 10:35:09 +0530

    Time: 1 ms (99.78%), Space: 47.1 MB (88.96%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int end = 0;
         int fardest = 0;
+        int end = 0;
         int cnt = 0;
-        for(int i = 0 ; i < n - 1 ; i++){
-            fardest  = Math.max(nums[i] + i , fardest);
+        for(int i = 0 ;i < n - 1; i++){
+            fardest = Math.max(fardest, nums[i] + i);
             if(i == end){
                 cnt++;
                 end = fardest;
             }
-            if(i >= n - 1) return cnt;
         }
         return cnt;
     }
