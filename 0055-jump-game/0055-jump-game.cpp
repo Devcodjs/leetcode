73c9e278d2f246167a2148bc07b09e9e0adb41d3
Juman Saikia <saikiajuman098@gmail.com>
2026-08-23 10:33:13 +0530

    Time: 0 ms (100%), Space: 52.5 MB (25.76%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,11 +3,11 @@ public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
         int fardest = 0;
-        for(int i = 0 ; i < n ;i++){
-            fardest = max(fardest , i + nums[i]);
-            if(fardest == n - 1) return true;
-            if(fardest == i) return false;
+        for(int i = 0 ; i < n ; i++){
+            fardest = max(fardest , nums[i] + i);
+            if(fardest >= n - 1) return true;
+            if(i == fardest) return false;
         }
-        return true;
+        return false;
     }
 };
