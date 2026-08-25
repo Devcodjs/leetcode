348f0b5da2ae8dcf487a40f8f5c5504f5bb27c7b
Juman Saikia <saikiajuman098@gmail.com>
2026-08-25 10:41:54 +0530

    Time: 0 ms (100%), Space: 17.6 MB (75.53%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -10,17 +10,17 @@ public:
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp1(n + 2 , 0) , dp2(n + 2 , 0);
+        vector<int> dp(n + 2 , 0);
         for(int i = n - 1 ; i >= 0 ; i--){
-            int onestep = cost[i] + dp1[i + 1];
-            int twostep = cost[i] + dp1[i + 2];
-            dp1[i] = min(onestep , twostep);
+            int onestep = cost[i] + dp[i + 1];
+            int twostep = cost[i] + dp[i + 2];
+            dp[i] = min(onestep , twostep);
         }
-        for(int i = n - 1 ; i >= 0 ; i--){
-            int onestep = cost[i] + dp2[i + 1];
-            int twostep = cost[i] + dp2[i + 2];
-            dp2[i] = min(onestep , twostep);
-        }
-        return min(dp1[0] , dp2[1]);
+        // for(int i = n - 1 ; i >= 0 ; i--){
+        //     int onestep = cost[i] + dp2[i + 1];
+        //     int twostep = cost[i] + dp2[i + 2];
+        //     dp2[i] = min(onestep , twostep);
+        // }
+        return min(dp[0] , dp[1]);
     }
 };
