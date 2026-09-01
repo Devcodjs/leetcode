a202a6b96f9eb17339077d95a2f3dfe15b81212b
Juman Saikia <saikiajuman098@gmail.com>
2026-09-01 15:20:16 +0530

    Time: 290 ms (65.21%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select max(distinct(salary)) as SecondHighestSalary  
+from Employee
+where salary <> (
+    select max(distinct(salary)) 
+    from Employee
+)
+limit 1;
